db6f142e9ff7a2efef1d26d19414b1266cad0c7d
zhuyinghao <coldrainsnow@163.com>
2023-09-06 10:55:17 +0800

    把markdown博客文章的标题全部下降一级，之前的一级变二级，二级变三级以此类推

---
+++ TitleDowngrade.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+int main() {
+	std::regex pattern(R"(^(#+)(\s\d+))");
+	std::string replacement = "$1#$2";
+
+	for (const auto& entry : fs::directory_iterator(fs::current_path())) {
+		if (entry.is_regular_file() && entry.path().extension() == ".md") {
+			std::ifstream input(entry.path());
+			std::string content((std::istreambuf_iterator<char>(input)), std::istreambuf_iterator<char>());
+			input.close();
+
+			content = std::regex_replace(content, pattern, replacement);
+
+			std::ofstream output(entry.path());
+			output << content;
+			output.close();
+		}
+	}
+
+	return 0;
+}
